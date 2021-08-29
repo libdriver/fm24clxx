e9508fb14a2d7afa4d0114454526f758b1cf0564
e <lishifenging@outlook.com>
2021-08-29 11:28:58 +0800

    fix: fix iic address bugs

--- src/driver_fm24clxx.h
+++ src/driver_fm24clxx.h
@@ -61,9 +61,9 @@ extern "C"{
  */
 typedef enum
 {
-    FM24CL04B  = 511,          /**< FM24CL04B type */
-    FM24CL16B  = 2047,         /**< FM24CL16B type */
-    FM24CL64B  = 8191,         /**< FM24CL64B type */
+    FM24CL04B = 511,          /**< FM24CL04B type */
+    FM24CL16B = 2047,         /**< FM24CL16B type */
+    FM24CL64B = 8191,         /**< FM24CL64B type */
 } fm24clxx_t;
 
 /**
@@ -71,14 +71,14 @@ typedef enum
  */
 typedef enum
 {
-    FM24CLXX_ADDRESS_A000 = 0,        /**< A0A1A2 000 */
-    FM24CLXX_ADDRESS_A001 = 1,        /**< A0A1A2 001 */
-    FM24CLXX_ADDRESS_A010 = 2,        /**< A0A1A2 010 */
-    FM24CLXX_ADDRESS_A011 = 3,        /**< A0A1A2 011 */
-    FM24CLXX_ADDRESS_A100 = 4,        /**< A0A1A2 100 */
-    FM24CLXX_ADDRESS_A101 = 5,        /**< A0A1A2 101 */
-    FM24CLXX_ADDRESS_A110 = 6,        /**< A0A1A2 110 */
-    FM24CLXX_ADDRESS_A111 = 7,        /**< A0A1A2 111 */
+    FM24CLXX_ADDRESS_A000 = 0,        /**< A2A1A0 000 */
+    FM24CLXX_ADDRESS_A001 = 1,        /**< A2A1A0 001 */
+    FM24CLXX_ADDRESS_A010 = 2,        /**< A2A1A0 010 */
+    FM24CLXX_ADDRESS_A011 = 3,        /**< A2A1A0 011 */
+    FM24CLXX_ADDRESS_A100 = 4,        /**< A2A1A0 100 */
+    FM24CLXX_ADDRESS_A101 = 5,        /**< A2A1A0 101 */
+    FM24CLXX_ADDRESS_A110 = 6,        /**< A2A1A0 110 */
+    FM24CLXX_ADDRESS_A111 = 7,        /**< A2A1A0 111 */
 } fm24clxx_address_t;
 
 /**
