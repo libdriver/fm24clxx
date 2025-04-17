d70912fdb9cc786f2259cae78ae3b98469eb52c0
lishifeng <lishifenging@outlook.com>
2025-04-17 22:51:04 +0800

    fix: fix doc errors

--- doc/mainpage/mainpage.h
+++ doc/mainpage/mainpage.h
@@ -1,10 +1,10 @@
 /**
  * @mainpage  LibDriver FM24CLXX
- * @brief     LibDriver FM24CLXX is a full function driver of FM24CLXX and it can run in both MCU and Linux platform.
+ * @brief     LibDriver FM24CLXX is a full-featured driver of FM24CLXX and it can run in both MCU and Linux platform.
  * @version   1.0.0
  * @author    Shifeng Li
  *
- * <h3>history</h3>
+ * <h3>History</h3>
  * <table>
  * <tr><th>Date        <th>Version  <th>Author      <th>Description
  * <tr><td>2021/06/15  <td>1.0      <td>Shifeng Li  <td>first upload
